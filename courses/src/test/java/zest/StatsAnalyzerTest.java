187fe2d779256b66d5834a873a79ad7e9d431a0b
LooPyt <luis_schmid@hotmail.com>
2022-04-24 01:11:06 +0200

    Working test without examination of boarder cases

--- courses/src/test/java/zest/StatsAnalyzerTest.java
+++ courses/src/test/java/zest/StatsAnalyzerTest.java
@@ -1,57 +1,70 @@
 package zest;
-
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 
 class StatsAnalyzerTest {
+    List<String> mockallcourses = new ArrayList<>();
+    List<String> Teachernames = new ArrayList<>();
+    List <Integer> ECTS = new ArrayList<>();
+
+
+    void addCourse( String CourseName, String Teachername, int numberofECTS){
+        mockallcourses.add(CourseName);
+        Teachernames.add(Teachername);
+        ECTS.add(numberofECTS);
+}
+    int sumECTS(List<Integer> List){
+        int sum = 0;
+        for (int i=0;i<List.size();i++){
+            sum+=List.get(i);
+        }
+        return sum;
+    }
+    List<String> distinctTeachers(List<String> Teacherlist){
+        List<String> dTeachers = Teacherlist;
+        for (int i=0; i<dTeachers.size()-1;i++){
+            for (int j= i+1; j<dTeachers.size(); j++){
+                if (dTeachers.get(i)==dTeachers.get(j)){
+                    dTeachers.remove(i);
+                }
+            }
+        }
+        return dTeachers;
+
+
+    }
     @Test
     void CountECTSforTeacher(){
-
-
-
-        //when(StatsAnalyzer.getTeachersECTS()).thenReturn(mymockECTS);
         CourseCurriculum mycuriculum = mock(CourseCurriculum.class);
+        addCourse("EST","Luis",3);
+        addCourse("CG","Marvin",6  );
+        addCourse("CGG","Marvin",2);
 
-        List<String> mockallcourses = new ArrayList<>();
-        List<String> Teachername = new ArrayList<>();
-        List <Integer> ECTS = new ArrayList<>();
-        
+        when(mycuriculum.allCourses()).thenReturn(mockallcourses);
+        for (int i =0; i<mockallcourses.size(); i++) {
+            when(mycuriculum.ectsCourse(mockallcourses.get(i))).thenReturn(ECTS.get(i));
+            when(mycuriculum.teacherCourse(mockallcourses.get(i))).thenReturn(Teachernames.get(i));
         }
+        StatsAnalyzer statsAnalyzer = new StatsAnalyzer(mycuriculum);
+        assertEquals(8, statsAnalyzer.getTeachersECTS("Marvin"));
+    }
+    @Test
+    void AverageByTeacher(){
+        CourseCurriculum curiculum = mock(CourseCurriculum.class);
+        addCourse("EST","Luis",3);
+        addCourse("CG","Marvin",6  );
+        addCourse("CGG","Marvin",2);
+        when(curiculum.allTeachers()).thenReturn(distinctTeachers(Teachernames));
+        when(curiculum.totalEcts()).thenReturn(sumECTS(ECTS));
 
-        String Teacher = "Luis";
-        String CourseTitle = "EST";
-        int ECTS = 6;
-        when(mycuriculum.addCourse(Teacher,CourseTitle,ECTS)).then(mycuriculum.ectsCourse(CourseTitle)==ECTS);
-
-
-        when(mycuriculum.allCourses()).thenReturn()
-
-
-
-
-
-        StatsAnalyzer statsAnalyzer = new StatsAnalyzer(CourseCurriculum mycuriculum);
-
-
-
-
-        curriculum.addCourse("Bagliatelli","EST",6);
-        curriculum.addCourse("Rolf","ITSecurity",3);
-        curriculum.addCourse("xyz","CG",3);
-        curriculum.addCourse("lsdjk","InfoII",6);
-        when(curriculum.addCourse(Teacher,CourseTitle,ECTS))
-
-        List<Course> listOfCourses = Arrays.asList(EST,ITSecurity,InfoII,CG);
-        when(curriculum.allCourses()).thenReturn(listOfCourses);
-        when(curriculum.teacherCourse(Course AnyCourse)).then
+        StatsAnalyzer statsAnalyzer = new StatsAnalyzer(curiculum);
+        assertEquals(5, statsAnalyzer.getAvgECTSByTeacher());
 
     }
 
